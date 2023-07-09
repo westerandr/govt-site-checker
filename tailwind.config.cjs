9375430110cf6093c8fcbab775ed8359cd69da7f
Andre Westerlund <andrewest_04@hotmail.com>
2023-07-09 19:19:47 +1200

    add theme colors, astro image and tailwind typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#16067f",
+
+					"secondary": "#fcde7e",
+
+					"accent": "#70f9d7",
+
+					"neutral": "#212c3b",
+
+					"base-100": "#ecedee",
+
+					"info": "#2198ed",
+
+					"success": "#0b6031",
+
+					"warning": "#b38b14",
+
+					"error": "#f62869",
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [require('daisyui')],
+	plugins: [require("@tailwindcss/typography"), require('daisyui')],
 }
