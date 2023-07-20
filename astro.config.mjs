ce506d2638dac99bd579d2413f723599f5237e3a
Andre Westerlund <andrewest_04@hotmail.com>
2023-07-21 08:52:05 +1200

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind(), image(), react()]
+  output: 'server',
+  integrations: [tailwind(), image(), react()],
+  adapter: netlify()
 });
