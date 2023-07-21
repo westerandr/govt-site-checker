f656d18d6a47d4e0813150264f7ab50c3b643962
Andre Westerlund <andrewest_04@hotmail.com>
2023-07-21 12:08:04 +1200

    fix: sync background and list item border colors, remove astro image

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -19,7 +19,7 @@ export default function List() {
     <button disabled={refresh} onClick={() => setRefresh(true)} className="btn bg-slate-800 text-white mb-8 w-36 hover:bg-slate-500">
       <svg className={refresh ? 'animate-spin' : ''} width="20px" height="20px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#fff" d="M7.248 1.307A.75.75 0 118.252.193l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 01-1.004-1.114l1.29-1.161a4.5 4.5 0 103.655 2.832.75.75 0 111.398-.546A6 6 0 118.018 2l-.77-.693z" /></svg>
       Refresh</button>
-    <ul role="list" className="divide-y divide-gray-100 m-w-7/12 lg:w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-11">
+    <ul role="list" className="divide-y divide-slate-50 m-w-7/12 lg:w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-11">
       {sitesData.sites.map((site, index) => (<ListItem key={index} title={site.name} url={site.site} refresh={refresh} />))}
     </ul>
   </div>
