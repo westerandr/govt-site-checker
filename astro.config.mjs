b015752e0a3eaca00eb2d43f977ef91d7ab893a3
Andre Westerlund <andrewest_04@hotmail.com>
2024-04-29 14:59:03 +1200

    upgrade astro & dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
