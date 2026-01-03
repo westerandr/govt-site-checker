ef16d390abbb10f29441b8036d2e0366c4f0a8aa
Andre Westerlund <andrewest_04@hotmail.com>
2026-01-03 14:05:45 +1300

    some more issues

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,11 +5,14 @@ module.exports = {
     {
       name: 'api',
       script: 'src/server.js',
+      cwd: __dirname,
       env: {
         NODE_ENV: 'production',
         PORT: process.env.BACKEND_PORT || 5000
       },
-
+      watch: false,
+      instances: 1,
+      exec_mode: 'fork',
     }
   ]
 }
